e6d84f47c447cbf43fc627e526d88a8cceaf5897
Localhost <aryakusuma94@gmail.com>
2012-05-20 08:21:18 +0700

    Proses Akun User

--- admin/system/proses_akun.php
+++ admin/system/proses_akun.php
@@ -1,19 +1,25 @@
 <?php
 include "../../configuration/config.php";
-
+@session_start();
 $full = isset($_POST['txtFull'])?$_POST['txtFull']:"";
 $user = isset($_POST['txtUser'])?$_POST['txtUser']:"";
 $pass = isset($_POST['txtPass'])?$_POST['txtPass']:"";
+$role = isset($_POST['role'])?$_POST['role']:"";
 
 $fullUpd = isset($_POST['txtFullUpd'])?$_POST['txtFullUpd']:"";
 $userUpd = isset($_POST['txtUserUpd'])?$_POST['txtUserUpd']:"";
 $passUpd = isset($_POST['txtPassUpd'])?$_POST['txtPassUpd']:"";
+$roleUpd = isset($_POST['roleUpd'])?$_POST['roleUpd']:"";
 $stts = isset($_POST['radYa'])?$_POST['radYa']:"";
+if ($roleUpd == '') {
+	$roleUpd = 'su_admin';
+	$stts    = '1';	
+}
 if(isset($_POST['save']))
 
 	{
 		$status= 1;
-		$q=mysql_query("INSERT INTO user_account(id_account,username_account,fullname_account,password_account,status_account) VALUES('','$user','$full','$pass','$status')");	
+		$q=mysql_query("INSERT INTO user_account(id_account,username_account,fullname_account,password_account,name_role,status_account) VALUES('','$user','$full','$pass','$role','$status')");	
 		if($q)
 		{
 			echo"<script>alert('Data Berhasil Disimpan');location='../index.php?page=dashboard&sub=akun'</script>";	
@@ -26,7 +32,7 @@ if(isset($_POST['save']))
 	
 if(isset($_POST['ubah_akun']))
 {
-	$sql=mysql_query("UPDATE user_account SET username_account = '$userUpd' , fullname_account = '$fullUpd' , password_account = '$passUpd' , status_account = '$stts' WHERE id_account = '".$_POST['id']."'");
+	$sql=mysql_query("UPDATE user_account SET username_account = '$userUpd' , fullname_account = '$fullUpd' , password_account = '$passUpd' , name_role = '$roleUpd' , status_account = '$stts' WHERE id_account = '".$_POST['id']."'");
 	if($sql)
 	{
 		echo"<script>location='../index.php?page=dashboard&sub=akun'</script>";	
