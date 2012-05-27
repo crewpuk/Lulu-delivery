019be200781a1113d208d4b962937e60d1ae828f
Rian Nugraha <calledme.rian@gmail.com>
2012-05-28 00:34:31 +0700

    change ribbon

--- ribbon.php
+++ ribbon.php
@@ -1,7 +1,7 @@
 <?php
 $link = array('transaksi_customer','transaksi_u_customer','input_customer[admin]','keluar');
-$title = array('Transaksi','Transaksi Customer','Data Customer','Keluar');
-$image = array('add.png','add.png','profil.png','cancel.png');
+$title = array('Transaksi','Persetujuan Transaksi','Data Customer','Keluar');
+$image = array('add.png','delivered.png','profil.png','cancel.png');
 
 //Panggil fungsi ribbon dengan memasukan array link, title, dan image
 ribbon($link,$title,$image);
